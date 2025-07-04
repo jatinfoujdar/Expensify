55fbb601615621556ba0e9f82d2f7f0b7a49b11e
Jatin Foujdar <jatin321001@gmail.com>
2025-07-04 14:37:25 +0530

    Initial Commit

---
+++ Expensify/ExpensifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensifyApp.swift
+//  Expensify
+//
+//  Created by jatin foujdar on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
