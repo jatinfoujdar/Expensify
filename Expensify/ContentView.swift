02f8547251d6c5bbf02e8b6987f5b0fef0768cd7
Jatin Foujdar <jatin321001@gmail.com>
2025-07-04 21:21:04 +0530

    commit

--- Expensify/ContentView.swift
+++ Expensify/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    //    @State private var expences = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                List{
+                    
+                }
+            }
+            
         }
-        .padding()
+        .navigationTitle("Expensify")
+        .toolbar{
+            
+        }
     }
 }
 
