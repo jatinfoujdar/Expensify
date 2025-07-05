77b0d29dee03c4d0befe33aae30ef84984907183
Jatin Foujdar <jatin321001@gmail.com>
2025-07-05 17:24:15 +0530

    personal section commit

--- Expensify/ContentView.swift
+++ Expensify/ContentView.swift
@@ -4,24 +4,52 @@
 //
 //  Created by jatin foujdar on 04/07/25.
 //
+//  ContentView.swift
+//  Expensify
+//
+//  Created by Jatin Foujdar on 04/07/25.
+//
 
 import SwiftUI
 
 struct ContentView: View {
 
-    //    @State private var expences = Expenses()
+    //    @State private var expenses = Expenses()
     
     var body: some View {
-        NavigationStack{
-            VStack{
-                List{
+        NavigationStack {
+            VStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Total Budget: ")
+                    // TextField("Total Budget", text: $totalBudgetText)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(.roundedBorder)
+                }
+                .padding()
+                
+                HStack {
+                    Image(systemName: remainingAmount() < 0 ? "exclamationmark.triangle.fill" : "checkmark.circle.fill")
+                        .foregroundColor(remainingAmount() < 0 ? .red : .green)
+                    
+                    Text("Remaining: \(remainingAmount(), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                        .font(.title2)
+                }
+                
+                List {
+                    Section(header: Text("Personal")){
+                        ForEach(expenses.items.filter{ $0.type == "Personal"}){item in
+                            expenseRow(for: item)
+                        }
+                        .onDelete{offsets in
+                            removeItems(for: "Personal" at: offsets)
+                        }
+                    }
                     
                 }
             }
-            
         }
         .navigationTitle("Expensify")
-        .toolbar{
+        .toolbar {
             
         }
     }
@@ -30,3 +58,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
